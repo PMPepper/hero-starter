ca498d44ad2477e6e42227d1e3f4e184a4aca776
PMPepper <pm_pepper@hotmail.com>
2015-01-24 18:09:53 +0000

    Second attempt at smarter AI
    New custom decision making. Still pretty primitive, but hopefully
    smarter than default!

--- hero.js
+++ hero.js
@@ -176,11 +176,182 @@ var moves = {
   // This hero will try really hard not to die.
   coward : function(gameData, helpers) {
     return helpers.findNearestHealthWell(gameData);
-  }
- };
+  },
+  
+    //TODO Need method to plan route while avoiding enemies, etc
+    custom : function( gameData, iHelpers){
+        //store on global variable to save me from having to passing around the whole time
+        helpers = iHelpers;
+        myHero = gameData.activeHero;
+        board = gameData.board;
+        
+        //Get stats on the nearest health well
+        var healthWellStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(boardTile) {
+            if (boardTile.type === 'HealthWell') {
+                return true;
+            }
+        });
+
+        //get nearest teammate
+        var teammateStats = helpers.findNearestObjectDirectionAndDistance(board, myHero, function(heroTile) {
+            return heroTile.type === 'Hero' && heroTile.team === myHero.team;
+        });
+        
+        //nearest unclaimed or enemy owned diamond mine
+        var diamondMineStats = helpers.findNearestObjectDirectionAndDistance(board, myHero, function(mineTile) {
+            if (mineTile.type === 'DiamondMine') {
+                if (mineTile.owner) {
+                    return mineTile.owner.team !== myHero.team;
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        });
+        
+        var adjacentEnemies = getAdjacentTiles( myHero.distanceFromLeft, myHero.distanceFromTop, filters.enemy );
+        
+        switch(adjacentEnemies.length ){
+            case 4://attack weakest enemy
+                return getWeakestAdjEnemyDirection( adjacentEnemies );
+            case 3://run away
+                var possibleDirections = [].concat( allDirections );//clone all directions array
+                
+                //remove directions which contain enemies
+                for( var i = 0; i < adjacentEnemies.length; i++ ){
+                    possibleDirections.splice( possibleDirections.indexOf( adjTileDirection( adjacentEnemies[i] ) ), 1 );
+                }
+                
+                //is the remaining direction accessible?
+                if( possibleDirections[0].type === 'Unoccupied'){
+                    return possibleDirections[0];
+                } else {
+                    //last ditch desparation
+                    return getWeakestAdjEnemyDirection( adjacentEnemies );
+                }
+            case 2:
+                var weakest = getWeakestEnemy( adjacentEnemies );
+                
+                if( healthWellStats && gameData.activeHero.health < 100 && healthWellStats.distance == 1){
+                    return healthWellStats.direction;//I'm next to a health well and hurt, just heal
+                } else if( weakest.health == 10 ){//I can just kill one
+                    return adjTileDirection( weakest );
+                } else {
+                    return healthWellStats.direction;//run to a health well
+                }
+            case 1:
+                if(adjacentEnemies[0].health == 10 ){//can I definately kill them?
+                    //move to them (and kill them!)
+                    return adjTileDirection( adjacentEnemies[0] );
+                } else {
+                    //TODO take judgement call on if attacking is a good idea
+                }
+                break;
+        }
+        
+        if( healthWellStats && gameData.activeHero.health < 100 && healthWellStats.distance === 1 ){
+            return healthWellStats.direction;//If I'm next to a health well and injured, heal.
+        } else if (gameData.activeHero.health <= 50){
+            return healthWellStats.direction;//If I am vunlerable, go and get healed
+        } else if( teammateStats && teammateStats.distance === 1 && teammateStats.health < 100 ){//if next to an injured team mate, heal them
+            return teammateStats.direction;
+        } else if( diamondMineStats && diamondMineStats.distance === 1 ){//if next to a diamond mine, grab it
+            return diamondMineStats.direction;
+        } else {
+            // Otherwise, go attack someone...anyone.
+            return helpers.findNearestEnemy(gameData);
+        }
+    }
+};
+
+
+var myHero, board, helpers, allDirections = ["North", "East", "South", "West"];
+
+function getTileDirection( dx, dy ){
+    if( dx === 1 ){
+        return "East";
+    } else if( dx === -1 ){
+        return "West";
+    } else if( dy === 1 ){
+        return "South";
+    } else if( dy === -1 ){
+        return "North";
+    }
+    
+    return false;
+}
+
+function getWeakestEnemy( enemies ){
+    if( enemies.length == 0 ){
+        return false;
+    }
+    
+    var lowestHealth = 100,
+        weakestEnemy = enemies[Math.floor(Math.random()*enemies.length)];
+    
+    for( var i = 0; i < enemies.length; i++ ){
+        if( enemies[i].health < lowestHealth ){
+            weakestEnemy = enemies[i];
+            lowestHealth = enemies[i].health;
+        }
+    }
+    
+    //TODO avoid attacking enemies next to health wells? Or that are/could be being healed?
+    
+    return weakestEnemy;
+}
+
+//TODO make this work even if not adjacent
+function getWeakestAdjEnemyDirection( enemies ){
+    if( enemies.length == 0 ){
+        return false;
+    }
+    
+    return adjTileDirection( getWeakestEnemy( enemies ) );
+}
+
+function adjTileDirection( tile ){
+    return getTileDirection( tile.distanceFromLeft - myHero.distanceFromLeft, tile.distanceFromTop - myHero.distanceFromTop );
+}
+
+function getAdjacentTiles( x, y, filter ){
+    var tiles = [];
+    
+    if (helpers.validCoordinates(board, x+1, y)) {
+        tiles.push( board.tiles[x+1][y] );
+    }
+    if (helpers.validCoordinates(board, x-1, y)) {
+        tiles.push( board.tiles[x-1][y] );
+    }
+    if (helpers.validCoordinates(board, x, y+1)) {
+        tiles.push( board.tiles[x][y+1] );
+    }
+    if (helpers.validCoordinates(board, x, y-1)) {
+        tiles.push( board.tiles[x][y-1] );
+    }
+    
+    //if supplied, filter out non applicable tiles
+    if( filter ){
+        for( var i = tiles.length-1; i > -1; --i ){
+            if( !filter( tiles[i] ) ){
+                tiles.splice( i, 1 );
+            }
+        }
+    }
+    
+    return tiles;
+}
+
+
+var filters = {
+    enemy:function( tile ){
+        return tile.type === 'Hero' && tile.team !== myHero.team;
+    }
+};
 
 //  Set our heros strategy
-var  move =  moves.aggressor;
+var  move =  moves.custom;
 
 // Export the move function here
 module.exports = move;
